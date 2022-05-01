4a13ac336fdb0b2ffb34b667b7f35b4376a8b7eb
Steve Korshakov <steve@korshakov.com>
2022-05-02 02:12:43 +0400

    feat: nice stack parser

--- src/api/handlers/handleAccountRun.ts
+++ src/api/handlers/handleAccountRun.ts
@@ -1,8 +1,27 @@
 import express from 'express';
 import { LiteClient } from 'ton-lite-client';
 import { log, warn } from "../../utils/log";
-import { Address, Cell } from 'ton';
-import { parseStack } from '../../parser/parseStack';
+import { Address, Cell, parseStack, StackItem } from 'ton';
+
+function stackToString(item: StackItem): any {
+    if (item.type === 'null') {
+        return { type: 'null' };
+    } else if (item.type === 'builder') {
+        return { type: 'builder', cell: item.cell.toBoc({ idx: false, crc32: false }).toString('base64') }
+    } else if (item.type === 'cell') {
+        return { type: 'cell', cell: item.cell.toBoc({ idx: false, crc32: false }).toString('base64') }
+    } else if (item.type === 'slice') {
+        return { type: 'slice', cell: item.cell.toBoc({ idx: false, crc32: false }).toString('base64') }
+    } else if (item.type === 'int') {
+        return { type: 'int', value: item.value.toString(10) }
+    } else if (item.type === 'nan') {
+        return { type: 'nan' }
+    } else if (item.type === 'tuple') {
+        return { type: 'tuple', items: item.items.map(stackToString) }
+    } else {
+        throw Error('Invalid item')
+    }
+}
 
 export function handleAccountRun(client: LiteClient): express.RequestHandler {
     return async (req, res) => {
@@ -12,18 +31,23 @@ export function handleAccountRun(client: LiteClient): express.RequestHandler {
             const command = req.params.command;
             const args = req.params.args as string | undefined;
             const parsedArgs = args && args.length > 0 ? Buffer.from(args, 'base64') : Buffer.alloc(0);
+            let stackArgs: StackItem[] = [];
+            if (parsedArgs.length > 0) {
+                stackArgs = parseStack(Cell.fromBoc(parsedArgs)[0]);
+            }
 
             // Fetch account state
             let mcInfo = (await client.lookupBlockByID({ seqno: seqno, shard: '-9223372036854775808', workchain: -1 }));
             let result = await client.runMethod(address, command, parsedArgs, mcInfo.id);
-            let resultParsed = result.result ? parseStack(Cell.fromBoc(Buffer.from(result.result!, 'base64'))[0].beginParse()) : null;
+            let resultParsed = result.result ? parseStack(Cell.fromBoc(Buffer.from(result.result!, 'base64'))[0]) : null;
 
             // Return data
             res.status(200)
                 .set('Cache-Control', 'public, max-age=31536000')
                 .send({
+                    arguments: stackArgs.map(stackToString),
+                    result: resultParsed ? resultParsed.map(stackToString) : null,
                     exitCode: result.exitCode,
-                    result: resultParsed,
                     resultRaw: result.result ? result.result : null,
                     block: {
                         workchain: result.block.workchain,

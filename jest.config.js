487acf2fea70c414d3c9586ac62d17bdffb80587
Yma Het <yma.het@gmail.com>
2025-06-12 17:20:20 +0600

    fix: Revive tests

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,11 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  testPathIgnorePatterns: ["/node_modules/","/dist/"]
+  testPathIgnorePatterns: ["/node_modules/","/dist/"],
+  transformIgnorePatterns: [
+    "node_modules/(?!(@ton|axios)/)"
+  ],
+  moduleNameMapper: {
+    '^axios$': require.resolve('axios')
+  }
 };
